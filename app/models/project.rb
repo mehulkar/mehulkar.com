b6761d604eed68720e689c50f098face7f2a039b
Mehul Kar <mehulkar@mochaleaf.com>
2013-05-22 12:02:13 -0700

    remove some extra things

--- app/models/project.rb
+++
@@ -1,4 +0,0 @@
-class Project < ActiveRecord::Base
-	has_many :posts
-	validates_presence_of :name
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Blog::Application.routes.draw do
   root to: 'static_pages#home'
-  
   resources :posts
-  resources :projects
-
-  match "/about" => 'static_pages#about'
-  match "/portfolio" => 'projects#index'
   match "/writes" => 'posts#index'
 end
