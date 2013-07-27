768a074e49b4e2558de66ce1e1bc79e4b6e1d5c3
Mehul Kar <mehul.kar@gmail.com>
2013-07-27 10:37:35 -0700

    remove untested code from model

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,47 +4,12 @@ class Post < ActiveRecord::Base
 
   after_create :tweet
 
-  # TODO: Make this not messy and not make assumptions about existing categories.
-  def categorize
-    if self.title.downcase.include? "quote" or self.title.include? "quotation"
-      self.category_id = Category.where(name: "Quotation").first.id
-    elsif self.title.downcase.include? "epiphany"
-      self.category_id = Category.where(name: "Epiphany").first.id
-    else
-      self.category_id = nil
-    end
-  end
-
   def formatted_title
     title.split(' ').map(&:capitalize).join(' ')
   end
-  # We want to add a number to titles if the post is categorized a quote or an epiphany
-  def adjust_title
-    if self.category
-      category = find_category_by_name(self.category.name)
 
-      last_post_in_category = last_assigned_post_in_category(category.id)
-      if last_post_in_category
-        num = last_post_in_category.title.split("#")[1].to_i + 1
-        self.title = category.name + " ##{num.to_s}"
-      else
-        self.title = category.name + " #1"
-      end
-    end
-  end
-
-  def find_category_by_name(name)
-    Category.where(name: name).first
-  end
-
-  def last_assigned_post_in_category(category_id)
-    posts = Post.where(category_id: category_id)
-    posts.last if !posts.empty?
-  end
-
+  # this is used for tweeting after create
   def url
-    # TODO use the host for this instead of hard coding.
-    # it's ok for now because we're only using it in one place
     "http://mehulkar.com/posts/#{self.id}"
   end
 
@@ -53,10 +18,12 @@ class Post < ActiveRecord::Base
   end
 
   def tweet
-    begin
-      Twitter.update(tweet_text)
-    rescue => e
-      puts "#{e.inspect} - #{tweet_text}"
+    if Rails.env.production?
+      begin
+        Twitter.update(tweet_text)
+      rescue => e
+        puts "#{e.inspect} - #{tweet_text}"
+      end
     end
   end
 end
