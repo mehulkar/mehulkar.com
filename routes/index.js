1810d88d89aba34a4da786b5cf9ef97f74ee2c49
Mehul Kar <mehul.kar@gmail.com>
2013-12-06 23:03:49 -0800

    switch to an express app instead of rails

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  res.render('index', { title: 'Home' });
+};
+
+exports.about = function(req, res){
+  res.render('about', { title: 'About' });
+};
