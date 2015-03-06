dab5a00d1479d7a8f888c67f10e1e5d6c0608e49
Mehul Kar <mehul.kar@apple.com>
2015-03-06 12:36:51 -0800

    Look for date override in file when building index

--- config.rb
+++ config.rb
@@ -49,8 +49,13 @@ helpers do
   end
 
   def first_created(path)
-    date = `git log --follow --date=short --pretty=format:%ad --diff-filter=A -- #{path}`
-    date = Date.today.to_s if date.empty?
+    date = `cat #{path} | grep date:`
+    if date.empty?
+      date = `git log --follow --date=short --pretty=format:%ad --diff-filter=A -- #{path}`
+      date = Date.today.to_s if date.empty?
+    else
+      date = date.split(" ")[1]
+    end
     date
   end
 end
