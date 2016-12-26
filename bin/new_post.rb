c9ad7c8a8c20095dd3059a71e6d8e1bf7ed24a1d
Mehul Kar <mehul.kar@gmail.com>
2016-12-26 02:51:20 -0800

    update new post script to put posts in subdirs

--- bin/new_post.rb
+++ bin/new_post.rb
@@ -3,12 +3,23 @@
 print "Name of post: "
 title = gets.chomp
 parameterized = title.split(" ").map(&:downcase).join('-')
-filename = "source/blog/#{parameterized}.md"
 
-file = File.open(filename, 'w+') do |f|
+now  = Time.now
+year = now.strftime('%Y')
+month = now.strftime('%m')
+full_date = now.strftime('%Y-%m-%d')
+
+dir = File.join('source', 'blog', year, month)
+
+# make sure directory exists
+FileUtils.mkdir_p(dir)
+
+filepath = File.join(dir, "#{parameterized}.md")
+
+file = File.open(filepath, 'w+') do |f|
   f.write("---\n")
   f.write("title: #{title}\n")
-  f.write("date: #{Time.now.strftime('%Y-%m-%d')}\n")
+  f.write("date: #{full_date}\n")
   f.write("categories: \n")
   f.write("---\n")
   f
