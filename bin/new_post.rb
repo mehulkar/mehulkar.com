b5d3f84a84689df96034e9cb5efcc60f83b4c924
Mehul Kar <mehul.kar@gmail.com>
2017-02-18 13:34:39 -0800

    Extract bin/new_post functionality into a class

--- bin/new_post.rb
+++ bin/new_post.rb
@@ -1,31 +1,17 @@
 #!/usr/bin/env ruby
 require 'fileutils'
+require_relative '../lib/post'
 
 print "Name of post: "
 title = gets.chomp
-parameterized = title.split(" ").map(&:downcase).join('-')
 
-now  = Time.now
-year = now.strftime('%Y')
-month = now.strftime('%m')
-full_date = now.strftime('%Y-%m-%d')
+puts "Creating new post"
+post = Post.new(title).tap(&:create)
 
-dir = File.join('source', 'blog', year, month)
+puts "Checking out new branch"
+`git checkout -b blog-#{post.parameterized_title}`
 
-# make sure directory exists
-FileUtils.mkdir_p(dir)
+puts "Opening #{post.file_path}"
+`subl #{post.file_path}`
 
-filepath = File.join(dir, "#{parameterized}.md")
-
-file = File.open(filepath, 'w+') do |f|
-  f.write("---\n")
-  f.write("title: #{title}\n")
-  f.write("date: #{full_date}\n")
-  f.write("categories: \n")
-  f.write("---\n")
-  f
-end
-
-`git checkout -b blog-#{parameterized}`
-
-`subl #{file.path}`
+# exit(0)
---
+++ lib/post.rb
@@ -0,0 +1,47 @@
+#!/usr/bin/env ruby
+require 'fileutils'
+
+class Post
+  attr_reader :file_path
+
+  def initialize(title, current_time=Time.now)
+    @title = title
+    @current_time = current_time
+  end
+
+  def parameterized_title
+    title.split(" ").map(&:downcase).join('-')
+  end
+
+  def create
+    dir = File.join('source', 'blog', year, month)
+    FileUtils.mkdir_p(dir)
+
+    @file_path = File.join(dir, "#{parameterized_title}.md")
+
+    file = File.open(@file_path, 'w+') do |f|
+      f.write("---\n")
+      f.write("title: #{title}\n")
+      f.write("date: #{full_date}\n")
+      f.write("categories: \n")
+      f.write("---\n")
+      f
+    end
+  end
+
+  private
+
+  attr_reader :title, :current_time
+
+  def year
+    current_time.strftime('%Y')
+  end
+
+  def month
+    current_time.strftime('%m')
+  end
+
+  def full_date
+    current_time.strftime('%Y-%m-%d')
+  end
+end
