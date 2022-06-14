929af59d0d274528523ee037114314662ed72437
Mehul Kar <mehul.kar@gmail.com>
2022-06-13 22:38:42 -0700

    Show latest watched film on home page
    - Add serverless function to get latest film from Letterboxed RSS feed
    - Fetch on client and display

---
+++ api/watching.mjs
@@ -0,0 +1,40 @@
+import Parser from 'rss-parser';
+
+const URL = 'https://letterboxd.com/mehulkar/rss/';
+
+
+async function getLatestFilm() {
+    const json = await new Parser().parseURL(URL);
+    const latestFilm = json.items[0];
+
+    console.log(latestFilm);
+
+    const { title } = latestFilm;
+
+    const STAR_CHAR = '★';
+
+    const matched = title.match(/(?<name>.*), (?<year>\d{4}) - (?<stars>★.*)/);
+
+    const { name, year, stars } =  matched.groups;
+
+    let rating = 0;
+    stars.split('').forEach(character => {
+        if (character === STAR_CHAR) {
+            rating++;
+        } else if (character === '½') {
+            rating += 0.5;
+        }
+    });
+
+    return {
+        name,
+        rating,
+        ratingString: stars,
+        year,
+    };
+}
+
+export default async function handler(req, res) {
+    const filmData = await getLatestFilm();
+    return res.send(JSON.stringify(filmData));
+  }
