249d33eab927ea27333704fe7b533cc5d9561e90
Mehul Kar <mehul.kar@gmail.com>
2022-06-24 14:45:49 -0700

    Fix formatting of watching.mjs file

--- api/watching.mjs
+++ api/watching.mjs
@@ -1,40 +1,39 @@
-import Parser from 'rss-parser';
+import Parser from "rss-parser";
 
-const URL = 'https://letterboxd.com/mehulkar/rss/';
-
+const URL = "https://letterboxd.com/mehulkar/rss/";
 
 async function getLatestFilm() {
-    const json = await new Parser().parseURL(URL);
-    const latestFilm = json.items[0];
+  const json = await new Parser().parseURL(URL);
+  const latestFilm = json.items[0];
 
-    console.log(latestFilm);
+  console.log(latestFilm);
 
-    const { title } = latestFilm;
+  const { title } = latestFilm;
 
-    const STAR_CHAR = '★';
+  const STAR_CHAR = "★";
 
-    const matched = title.match(/(?<name>.*), (?<year>\d{4}) - (?<stars>★.*)/);
+  const matched = title.match(/(?<name>.*), (?<year>\d{4}) - (?<stars>★.*)/);
 
-    const { name, year, stars } =  matched.groups;
+  const { name, year, stars } = matched.groups;
 
-    let rating = 0;
-    stars.split('').forEach(character => {
-        if (character === STAR_CHAR) {
-            rating++;
-        } else if (character === '½') {
-            rating += 0.5;
-        }
-    });
+  let rating = 0;
+  stars.split("").forEach((character) => {
+    if (character === STAR_CHAR) {
+      rating++;
+    } else if (character === "½") {
+      rating += 0.5;
+    }
+  });
 
-    return {
-        name,
-        rating,
-        ratingString: stars,
-        year,
-    };
+  return {
+    name,
+    rating,
+    ratingString: stars,
+    year,
+  };
 }
 
 export default async function handler(req, res) {
-    const filmData = await getLatestFilm();
-    return res.send(JSON.stringify(filmData));
-  }
+  const filmData = await getLatestFilm();
+  return res.send(JSON.stringify(filmData));
+}
