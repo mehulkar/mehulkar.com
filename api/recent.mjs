00b39ce39f5b1fe34a2ce45c9877642f8b59d829
Mehul Kar <mehul.kar@gmail.com>
2022-12-12 23:42:32 -0800

    500 when things go wrong

--- api/recent.mjs
+++ api/recent.mjs
@@ -3,15 +3,18 @@ import { getAnnotation } from "./_lib/feedly-api.mjs";
 import { getTweet } from "./_lib/twitter-api.mjs";
 
 export default async function handler(req, res) {
-  const [film, annotation, tweet] = await Promise.all([
-    getFilm(),
-    getAnnotation(),
-    getTweet(),
-  ]);
-
-  return res.status(200).json({
-    film,
-    annotation,
-    tweet,
-  });
+  try {
+    const [film, annotation, tweet] = await Promise.all([
+      getFilm(),
+      getAnnotation(),
+      getTweet(),
+    ]);
+    return res.status(200).json({
+      film,
+      annotation,
+      tweet,
+    });
+  } catch (e) {
+    return res.status(500).json({ error: e });
+  }
 }
